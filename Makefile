64ea365eecdcb839ab1ed08eeab22dec5d22d2dc
ecyht2 <ecyht2@nottingham.edu.my>
2022-03-22 16:33:44 +0800

    Updated Makefile

--- Makefile
+++ Makefile
@@ -8,4 +8,9 @@ source = source ~/project/env/bin/activate &&
 robot: robot.py
 	${source} python3 robot.py
 
+line_following: line_following.py
+	${source} python3 line_following.py
+
+shape_detection: shape_detection.py
+	${source} python3 shape_detection.py
 # end
--- robot.py
+++ robot.py
@@ -3,7 +3,7 @@ import RPi.GPIO as GPIO
 from time import sleep, time
 from classes import *
 
-right_wheel = (12, 21, 20)
+right_wheel = (21, 12, 20)
 left_wheel = (18, 24, 23)
 
 car = Car(left_wheel, right_wheel, (26, 6.5, 20), (19, 6.5, 20))
@@ -21,7 +21,7 @@ def setup():
 def loop():
     cTime = time() + 2
     while True:
-        car.forward(40)
+        car.forward()
         encoderLeft.update()
         encoderRight.update()
         if time() >= cTime:
