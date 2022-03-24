157e9d1b207841acd68e041603084788b5d22fa2
ecyht2 <ecyht2@nottingham.edu.my>
2022-03-24 16:35:32 +0800

    Optimized K_P and K_D values

--- line_following.py
+++ line_following.py
@@ -28,72 +28,79 @@ rawCapture = PiRGBArray(camera,size=(192, 108))
 time.sleep(0.1)
 
 # setup GPIO pins
-robot.setup()
 car = robot.car
 
 # Initialize PID values
-K_P = 0.15
+K_P = 0.25
 K_I = 0
-K_D = 0
+K_D = 0.25
 baseline = 96
 basespeed = 30
 pid = PID(baseline, K_P, K_I, K_D)
 
-# Loop over all frames captured by camera indefinitely
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+def loop():
+	# Loop over all frames captured by camera indefinitely
+	for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
 
-	# Display camera input
-	image = frame.array
-	# Flipping the image
-	image = cv2.flip(image, 0)
-	image = cv2.flip(image, +1)
-	#image = image[15:]
+		# Display camera input
+		image = frame.array
+		# Flipping the image
+		image = cv2.flip(image, 0)
+		image = cv2.flip(image, +1)
+		#image = image[15:]
 
-	# Create key to break for loop
-	key = cv2.waitKey(1) & 0xFF
+		# Create key to break for loop
+		key = cv2.waitKey(1) & 0xFF
 
-	# convert to grayscale, gaussian blur, and threshold
-	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-	blur = cv2.GaussianBlur(gray,(5,5),0)
-	ret,thresh = cv2.threshold(blur, 30, 70, cv2.THRESH_BINARY_INV)
+		threshold = [0, 0]
+		with open("/home/pi/project/threshold.txt", "r") as f:
+			threshold[0] = int(f.readline())
+			threshold[1] = int(f.readline())
 
-	# Erode to eliminate noise, Dilate to restore eroded parts of image
-	mask = cv2.erode(thresh, None, iterations=2)
-	mask = cv2.dilate(mask, None, iterations=2)
+		# convert to grayscale, gaussian blur, and threshold
+		gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+		blur = cv2.GaussianBlur(gray,(5,5),0)
+		ret,thresh = cv2.threshold(blur, threshold[0], threshold[1], cv2.THRESH_BINARY_INV)
 
-	# Find all contours in frame
-	contours, hierarchy = cv2.findContours(mask.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-	#cv2.imshow('mask', mask)
+		# Erode to eliminate noise, Dilate to restore eroded parts of image
+		mask = cv2.erode(thresh, None, iterations=2)
+		mask = cv2.dilate(mask, None, iterations=2)
 
-
-	# Find x-axis centroid of largest contour and cut power to appropriate motor
-	# to recenter camera on centroid.
-	# This control algorithm was written referencing guide:
-	# Author: Einsteinium Studios
-	# Availability: http://einsteiniumstudios.com/beaglebone-opencv-line-following-robot.html
-	if len(contours) > 0:
-		# Find largest contour area and image moments
-		c = max(contours, key = cv2.contourArea)
-		M = cv2.moments(c)
-
-		# Find x-axis centroid using image moments
-		cx = int(M['m10']/M['m00'])
-
-		# Updating PID
-		pid.update(cx)
-		PID = pid.get_PID()
-
-#		# Moving Car
-		car.move_car(basespeed - PID, basespeed + PID)
-		print(PID, cx)
+		# Find all contours in frame
+		contours, hierarchy = cv2.findContours(mask.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
 
-		#cv2.putText(image, str(cx), (0, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+		# Find x-axis centroid of largest contour and cut power to appropriate motor
+		# to recenter camera on centroid.
+		# This control algorithm was written referencing guide:
+		# Author: Einsteinium Studios
+		# Availability: http://einsteiniumstudios.com/beaglebone-opencv-line-following-robot.html
+		if len(contours) > 0:
+			# Find largest contour area and image moments
+			c = max(contours, key = cv2.contourArea)
+			M = cv2.moments(c)
 
-	#cv2.imshow('image', image)
-	if key == ord("q"):
-            break
+			# Find x-axis centroid using image moments
+			cx = int(M['m10']/M['m00'])
 
-	rawCapture.truncate(0)
+			# Updating PID
+			pid.update(cx)
+			PID = pid.get_PID()
 
-robot.exit()
+			# Moving Car
+			car.move_car(basespeed - PID, basespeed + PID)
+			print(PID, cx)
+
+		#cv2.imshow('mask', mask)
+		#cv2.imshow('image', image)
+		if key == ord("q"):
+			break
+
+		rawCapture.truncate(0)
+
+if __name__ == '__main__':
+	try:
+		robot.setup()
+		loop()
+	finally:
+		robot.exit()
