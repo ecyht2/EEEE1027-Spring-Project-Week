0500d2f06b6ad3672c93816642a8d3967f6b90cc
ecyht2 <ecyht2@nottingham.edu.my>
2023-02-12 12:14:42 +0800

    Added bluetooth files

---
+++ src/raspberry-car/rfcomm_server.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+"""A simple Python script to receive messages from a client over
+Bluetooth using Python sockets (with Python 3.3 or above).
+"""
+# https://community.appinventor.mit.edu/t/raspberry-pi-bluetooth-send-receive/59846
+# https://people.csail.mit.edu/albert/bluez-intro/
+import signal
+import socket
+import sys
+import time
+from multiprocessing import Process, queues
+from types import NoneType
+from typing import NoReturn
+
+import dbus
+
+import sdp
+
+
+def get_default_bluetooth() -> str:
+    """Gets the default bluetooth device of the OS.
+
+    This function relies on bluez to get the default bluetooth device. Please
+    ensure bluez is installed in the system. It also uses dbus to interface
+    with bluez. (dbus should be installed if bluez is instaleld)
+
+    :return: The default bluetooth device address.
+    """
+    # Fetching bus
+    bus = dbus.SystemBus()
+    bluez_bus = bus.get_object('org.bluez', '/org/bluez/hci0')
+
+    # Fetching Address property
+    props_iface = dbus.Interface(bluez_bus, 'org.freedesktop.DBus.Properties')
+    address = props_iface.Get('org.bluez.Adapter1', 'Address')
+
+    return address
+
+
+def recieve_data(bluetooth_address: str | NoneType = "", channel: int = 1,
+                 output: queues.Queue | NoneType = None) -> NoReturn:
+    """Create a scoket to receive data from a bluetooth device from channel 1.
+
+    The RFCOMM protocol is used for the communication.
+
+    The function will raise a ValueError if the RFCOMM is not within 1-30.
+    It also raises a ValueError if channel is not an integer.
+
+    :param bluetooth_address: The address of the bluetooth device to be used.
+    If an empty string or None is provided, the default bluetooth device of the
+    OS is used. (bluez is required for device discovery)
+    :param output: The output queue to transfer data to. If None is provided,
+    the data would be printed to standard output.
+    """
+    # Setting up bluetooth address
+    if bluetooth_address is None or bluetooth_address == "":
+        HOST = get_default_bluetooth()
+    else:
+        HOST = bluetooth_address
+
+    # Setting up RFCOMM channel
+    if not isinstance(channel, int):
+        raise ValueError("channels needs to be an integer.")
+    if channel < 1 or channel > 30:
+        raise ValueError("RFCOMM channel must be between 1 and 30.")
+    PORT = channel
+
+    print(f"Recieving Data from channel {channel}")
+
+    with socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM,
+                       socket.BTPROTO_RFCOMM) as s:
+        # Connecting to client
+        s.bind((HOST, PORT))
+        s.listen(1)
+        client, addr = s.accept()
+        print(f"Connected with {addr}")
+
+        while True:
+            # Recieving and decoding data
+            data = client.recv(1)
+            data = data.decode()
+
+            # Outputting data
+            if output is None:
+                print(f"Recived {data} from client")
+            else:
+                output.put(data)
+
+
+if __name__ == '__main__':
+    thread_advertise = Process(target=sdp.advertise_service, args=(1,))
+    thread_socket = Process(target=recieve_data, args=("",))
+
+    signal.signal(signal.SIGTERM, lambda a, b: sys.exit(0))
+    signal.signal(signal.SIGINT, lambda a, b: sys.exit(0))
+
+    try:
+        thread_advertise.start()
+        thread_socket.start()
+        while thread_advertise.is_alive() and thread_socket.is_alive():
+            time.sleep(1)
+    finally:
+        print("Shutting Down")
+        thread_advertise.terminate()
+        thread_socket.terminate()
---
+++ src/raspberry-car/sdp.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+"""A simple script that registers a bluez profile that enables SPP.
+
+This script is based on spp.py script on
+https://scribles.net/setting-up-bluetooth-serial-port-profile-on-raspberry-pi-using-d-bus-api/
+"""
+# https://stackoverflow.com/questions/53069598/bluez-adding-services-attributes-and-profiles-without-sdptool-command
+# https://stackoverflow.com/questions/41290114/bluetooth-sdp-where-is-sdpd
+# https://scribles.net/setting-up-bluetooth-serial-port-profile-on-raspberry-pi-using-d-bus-api/
+import signal
+import sys
+import time
+from multiprocessing import Process
+
+import dbus
+
+
+def advertise_service(channel: int = 1):
+    """Sets-up a bluez profile to advertise an RFCOMM service at a given
+    channel.
+
+    The function will raise a ValueError if the RFCOMM is not within 1-30.
+    It also raises a ValueError if channel is not an integer.
+
+    :param channel: The channel of the RFCOMM service.
+    """
+    if not isinstance(channel, int):
+        raise ValueError("channels needs to be an integer.")
+    if channel < 1 or channel > 30:
+        raise ValueError("RFCOMM channel must be between 1 and 30.")
+
+    print(f"Advertising RFCOMM Service in channel {channel}")
+
+    # https://github.com/AnesBenmerzoug/Bluetooth_HID/blob/master/sdp_record.xml
+    # https://btprodspecificationrefs.blob.core.windows.net/assigned-numbers/Assigned%20Number%20Types/Assigned%20Numbers.pdf
+    # http://www.bluecove.org/bluecove/apidocs/javax/bluetooth/ServiceRecord.html
+    service_record = """
+    <?xml version="1.0" encoding="UTF-8" ?>
+    <record>
+    <attribute id="0x0001">
+        <sequence>
+        <uuid value="0x1101"/>
+        </sequence>
+    </attribute>
+    <attribute id="0x0004">
+        <sequence>
+        <sequence>
+            <uuid value="0x0003"/>
+            <uint8 value="1" name="channel"/>
+        </sequence>
+        </sequence>
+    </attribute>
+    </record>
+    """
+
+    bus = dbus.SystemBus()
+    manager = dbus.Interface(bus.get_object("org.bluez", "/org/bluez"),
+                             "org.bluez.ProfileManager1")
+    manager.RegisterProfile("/bluez",
+                            "00001101-0000-1000-8000-00805f9b34fb",
+                            {
+                                "AutoConnect": True,
+                                "ServiceRecord": service_record
+                            })
+    while True:
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    process = Process(target=advertise_service)
+    signal.signal(signal.SIGTERM, lambda a, b: sys.exit(0))
+    signal.signal(signal.SIGINT, lambda a, b: sys.exit(0))
+
+    try:
+        process.start()
+        process.join()
+    finally:
+        print("Stopped Advertising Service")
+        process.terminate()
